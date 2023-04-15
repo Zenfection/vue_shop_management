474336d536d595481981718ca4bd87655d07ab41
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2023-04-15 23:05:13 +0700

    update

--- frontend/src/global/api.js
+++ frontend/src/global/api.js
@@ -1,5 +1,6 @@
 const api = {
-    backend: "http://localhost:3000"
+    // check import.meta.env.NODE_ENV
+    backend: import.meta.env.NODE_ENV === 'development' ? 'http://localhost:3000' : import.meta.env.VITE_BACKEND_API
 }
 
 export default api
